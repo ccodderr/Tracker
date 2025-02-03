70d915d7e485fbb15e4bee8c16b375f143e37ba8
Iana <crptcharm@gmail.com>
2025-02-03 22:15:19 +0200

    Свёрстан экран запуска LaunchScreen

---
+++ Tracker/LaunchScreen.swift
@@ -0,0 +1,33 @@
+//
+//  LaunchScreen.swift
+//  Tracker
+//
+//  Created by Yana Silosieva on 03.02.2025.
+//
+
+import UIKit
+
+final class LaunchScreen: UIViewController {
+    
+    //    MARK: Properties
+    private lazy var image = UIImageView(image: UIImage(named: "Logo"))
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    //    MARK: Methods
+    private func setupView() {
+        view.backgroundColor = .blue
+        image.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(image)
+        
+        NSLayoutConstraint.activate([
+            image.widthAnchor.constraint(equalToConstant: 91),
+            image.heightAnchor.constraint(equalToConstant: 94),
+            image.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            image.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+    }
+}
